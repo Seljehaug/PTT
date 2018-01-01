9bd621b9a81b3569179cb2a27b15de6be34425ee
Seljehaug <cs_seljehaug@hotmail.com>
2018-01-01 12:22:10 +0100

    collapsible panels list

--- js/collapsible-panels.js
+++ js/collapsible-panels.js
@@ -4,10 +4,10 @@ $(function(){
 	$panel.each(function(i){
 		var index = i + 1;
 		$(this).attr('id', 'panel-' + index);
-		console.log($(this));
 	});
 
 	$panel.on('click', function(){
+		$(this).toggleClass('active');
 		$(this).find('.panel-content').toggleClass('is-open');
 	});
 });
