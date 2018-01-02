ab518924e88e6067f8b24c9590db4fd413d452d5
Seljehaug <cs_seljehaug@hotmail.com>
2018-01-02 14:06:00 +0100

    mobile hover fix on collapsible panels

--- js/collapsible-panels.js
+++ js/collapsible-panels.js
@@ -7,7 +7,15 @@ $(function(){
 	});
 
 	$panel.on('click', function(){
+		$panel.removeClass('mobile-hover-fix');
+
 		$(this).toggleClass('active');
 		$(this).find('.panel-content').toggleClass('is-open');
+
+		// Remove the hover effect on mobile when clicking on a panel that was active
+		// The hover state on mobile would not be removed until clicking elsewhere
+		if(! $(this).hasClass('active') ){
+			$(this).toggleClass('mobile-hover-fix');
+		}
 	});
 });
